f33caa06d8150bcd1bbf8c86fd92c79e8124fbe3
U-DELAWARE\MUSIEP1 <MUSIEP1@PA2-MUSIEP1-L1.us.ad.lfg.com>
2015-03-06 18:05:54 -0500

    params.pp fix "Cannot reassign variable MACsString"

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,20 +4,26 @@ class sshdmn::params {
       $server_package_name = 'openssh-server'
       $sshd_config = '/etc/ssh/sshd_config'
       $service_name = 'sshd'
-
-      $MACsString = 'MACs hmac-sha1,umac-64@openssh.com,hmac-ripemd160'
-      if $::operatingsystemmajrelease < 6 {
+      if $::operatingsystemmajrelease > 5 {
+        $MACsString = 'MACs hmac-sha1,umac-64@openssh.com,hmac-ripemd160'
+      } else {
         $MACsString = 'MACs hmac-sha1,hmac-ripemd160'
-      } 
+      }
     }
+
     AIX: {
       $server_package_name = 'openssh-server'
       $sshd_config = '/etc/ssh/sshd_config'
       $service_name = 'sshd'
       $MACsString = 'MACs hmac-sha2-256,hmac-sha2-512,hmac-sha1'
     }
+
     default: {
-      fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+      $server_package_name = 'openssh-server'
+      $sshd_config = '/etc/ssh/sshd_config'
+      $service_name = 'sshd'
+      $MACsString = 'MACs hmac-sha1,umac-64@openssh.com,hmac-ripemd160'
+      ###fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
   }
 
