221bbd62dfe4056669f202e4366b3cd32c6e5bbf
pamus2k <pamus2k@gmail.com>
2015-03-28 11:23:54 -0400

    sshdmn class changed to use augeas instead of file_line

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,67 @@
-# == Class: sshdmn
-#
-# Full description of class sshdmn here.
-#
-# === Parameters
-# MACsLine
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*MACsLine*]
-# MAC (Message Authentication Code) algorithm(s) used for data integrity verification
-# String of comma separated parameters
-#
-# === Examples
-# MACs hmac-sha1,umac-64@openssh.com,hmac-ripemd160
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
-
-class sshdmn ( $MACsLine = $sshdmn::params::MACsString ) inherits sshdmn::params {
+class sshdmn inherits sshdmn::params {
   file { $sshdmn::params::sshd_config:
       ensure => present,
       mode => 600,
-  } ->
-  file_line { $sshdmn::params::sshd_config:
-      line => $MACsLine,
-      match => '^MACs ', # must match line as well
-      ensure => present,
-      multiple => true,  # just in case you have same parameter listed multiple times - change all lines
-      path => $sshdmn::params::sshd_config,
-      notify  => Service[$sshdmn::params::service_name],
   }
 
+### Apply sshd configuration changes using augeas
+  case $::osfamily {
+    RedHat: {
+      if $::operatingsystemmajrelease > 5 {
+         augeas { "sshd_config_params":
+           context => "/files${sshdmn::params::sshd_config}",
+           changes => [
+                     "set MACs/1 hmac-sha1",
+                     "set MACs/2 umac-64@openssh.com",
+                     "set MACs/3 hmac-ripemd160",
+                     "set Protocol 2",
+                     "set PermitRootLogin without-password",
+           ],
+           notify  => Service[$sshdmn::params::service_name],
+         }
+
+       } else {
+         augeas { "sshd_config_params":
+           context => "/files${sshdmn::params::sshd_config}",
+           changes => [
+                     "set MACs/1 hmac-sha1",
+                     "set MACs/2 hmac-ripemd160",
+                     "set Protocol 2",
+                     "set PermitRootLogin without-password",
+           ],
+           notify  => Service[$sshdmn::params::service_name],
+         }
+       }
+
+    }
+    AIX: {
+         augeas { "sshd_config_params":
+           context => "/files${sshdmn::params::sshd_config}",
+           changes => [
+                     "set MACs/1 hmac-sha2-256",
+                     "set MACs/2 hmac-sha2-512",
+                     "set MACs/3 hmac-sha1",
+           ],
+           notify  => Service[$sshdmn::params::service_name],
+         }
+    }
+    default: {
+         augeas { "sshd_config_params":
+           context => "/files${sshdmn::params::sshd_config}",
+           changes => [
+                     "set MACs/1 hmac-sha1",
+                     "set MACs/2 umac-64@openssh.com",
+                     "set MACs/3 hmac-ripemd160",
+                     "set Protocol 2",
+                     "set PermitRootLogin without-password",
+           ],
+           notify  => Service[$sshdmn::params::service_name],
+         }
+      ###fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+    }
+  }
+
+### make sure sshd service running
   service { $sshdmn::params::service_name:
     ensure => running,
     enable => true,
@@ -53,10 +70,9 @@ class sshdmn ( $MACsLine = $sshdmn::params::MACsString ) inherits sshdmn::params
     subscribe =>  File[$sshdmn::params::sshd_config],
   }
 
+### make sure software package installed
   package { $sshdmn::params::server_package_name:
     ensure => present,
     before => File[$sshdmn::params::sshd_config],
   }
-
 }
-
