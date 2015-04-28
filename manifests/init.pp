bb6b587a8f5c4feb1c3920b60cd1ea7682abe9dc
Paul Musienko <musiep1@nc2tlenv01.lnc.com>
2015-04-28 09:21:14 -0400

    sshd safe configuration change with validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,22 @@
 class sshdmn inherits sshdmn::params {
-  file { $sshdmn::params::sshd_config:
-      ensure => present,
-      mode => 600,
+### set and control sshd_config with validation of the parameters ###
+
+  # sshd_config changes are done against below processing file to be validated
+  $proc_file = "${sshdmn::params::sshd_config}.puptmp"
+  file {  $proc_file:
+    ensure => file,
+    source => $sshdmn::params::sshd_config,
   }
 
-### Apply sshd configuration changes using augeas
+### Apply sshd configuration changes using augeas to the proc_file
   case $::osfamily {
     RedHat: {
       if $::operatingsystemmajrelease > 5 {
          augeas { "sshd_config_params":
-           context => "/files${sshdmn::params::sshd_config}",
+           incl => "${proc_file}",
+           lens => "Sshd.lns",
+           context => "/files${proc_file}",
+           ##context => "/files${sshdmn::params::sshd_config}",
            changes => [
                      "set MACs/1 hmac-sha1",
                      "set MACs/2 umac-64@openssh.com",
@@ -17,37 +24,49 @@ class sshdmn inherits sshdmn::params {
                      "set Protocol 2",
                      "set PermitRootLogin without-password",
            ],
-           notify  => Service[$sshdmn::params::service_name],
+           ##notify  => Service[$sshdmn::params::service_name],
+           ##notify => Exec['validate_ssh'],
          }
 
        } else {
          augeas { "sshd_config_params":
-           context => "/files${sshdmn::params::sshd_config}",
+           incl => "${proc_file}",
+           lens => "Sshd.lns",
+           context => "/files${proc_file}",
+           ##context => "/files${sshdmn::params::sshd_config}",
            changes => [
                      "set MACs/1 hmac-sha1",
                      "set MACs/2 hmac-ripemd160",
                      "set Protocol 2",
                      "set PermitRootLogin without-password",
            ],
-           notify  => Service[$sshdmn::params::service_name],
+           ##notify  => Service[$sshdmn::params::service_name],
+           ##notify => Exec['validate_ssh'],
          }
        }
 
     }
     AIX: {
          augeas { "sshd_config_params":
-           context => "/files${sshdmn::params::sshd_config}",
+           incl => "${proc_file}",
+           lens => "Sshd.lns",
+           context => "/files${proc_file}",
+           ##context => "/files${sshdmn::params::sshd_config}",
            changes => [
                      "set MACs/1 hmac-sha2-256",
                      "set MACs/2 hmac-sha2-512",
                      "set MACs/3 hmac-sha1",
            ],
-           notify  => Service[$sshdmn::params::service_name],
+           ##notify  => Service[$sshdmn::params::service_name],
+           ##notify => Exec['validate_ssh'],
          }
     }
     default: {
          augeas { "sshd_config_params":
-           context => "/files${sshdmn::params::sshd_config}",
+           incl => "${proc_file}",
+           lens => "Sshd.lns",
+           context => "/files${proc_file}",
+           ##context => "/files${sshdmn::params::sshd_config}",
            changes => [
                      "set MACs/1 hmac-sha1",
                      "set MACs/2 umac-64@openssh.com",
@@ -55,10 +74,29 @@ class sshdmn inherits sshdmn::params {
                      "set Protocol 2",
                      "set PermitRootLogin without-password",
            ],
-           notify  => Service[$sshdmn::params::service_name],
+           ##notify  => Service[$sshdmn::params::service_name],
+           ##notify => Exec['validate_ssh'],
          }
       ###fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
+  }
+
+  ##exec { 'validate_ssh':
+  ##  path => ['/usr/sbin', '/usr/bin', '/sbin'],
+  ##  command      => "sshd -tf ${proc_file}",
+  ##  require     => File[$proc_file],
+  ##  refreshonly => true,
+  ## }
+
+### Validate configuration before replacing sshd_config file
+  file { $sshdmn::params::sshd_config:
+    ensure  => file,
+    source  => $proc_file,
+    mode => 600,
+    owner => root,
+    validate_cmd => "${sshdmn::params::sshd_exe} -tf %",
+    ##require => Exec['validate_ssh'],
+    notify  => Service[$sshdmn::params::service_name],
   }
 
 ### make sure sshd service running
@@ -75,4 +113,6 @@ class sshdmn inherits sshdmn::params {
     ensure => present,
     before => File[$sshdmn::params::sshd_config],
   }
-}
+
+} # end class sshdmn
+
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,16 +3,19 @@ class sshdmn::params {
     RedHat: {
       $server_package_name = 'openssh-server'
       $sshd_config = '/etc/ssh/sshd_config'
+      $sshd_exe = '/usr/sbin/sshd'
       $service_name = 'sshd'
     }
     AIX: {
       $server_package_name = 'openssh-server'
       $sshd_config = '/etc/ssh/sshd_config'
+      $sshd_exe = '/usr/sbin/sshd'
       $service_name = 'sshd'
     }
     default: {
       $server_package_name = 'openssh-server'
       $sshd_config = '/etc/ssh/sshd_config'
+      $sshd_exe = '/usr/sbin/sshd'
       $service_name = 'sshd'
       ###fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
